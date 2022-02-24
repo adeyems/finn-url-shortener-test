13f9f8bc283a98ff56df386fe6b6cbd71df73682
ADEYEMO Qudus <adeyems@gmail.com>
2022-02-24 12:44:06 +0100

    modified check for encode test

--- _tests_/functional.spec.js
+++ _tests_/functional.spec.js
@@ -46,4 +46,19 @@ describe('the tests for testing api full functionalities', () => {
             done();
         });
     });
+    it('should return a 201 created response and an encoded shortened URL', async (done) => {
+        request()
+            .post('/api/v1/encode')
+            .send({ url: "https://google.com" })
+            .expect('Content-Type', /json/)
+            .expect(201)
+            .end((err, res) => {
+            if (err)
+                return done(err);
+            expect(res.body).toMatchObject({ status: "success", message: "Encoded URL" });
+            expect(res.body.data).toHaveProperty("url", expect.stringMatching(/https?:\/\/(?:w{1,3}\.)?[^\s.]+(?:\.[a-z]+)*(?::\d+)?(?![^<]*(?:<\/\w+>|\/?>))/));
+            expect(res.body.data.url.split('/')[3].length).toEqual(6);
+            done();
+        });
+    });
 });
--- _tests_/functional.spec.ts
+++ _tests_/functional.spec.ts
@@ -45,4 +45,18 @@ describe('the tests for testing api full functionalities', () => {
             })
     })
 
+    it('should return a 201 created response and an encoded shortened URL', async done => {
+        request()
+            .post('/api/v1/encode')
+            .send({url: "https://google.com"})
+            .expect('Content-Type', /json/)
+            .expect(201)
+            .end((err, res) => {
+                if (err) return done(err)
+                expect(res.body).toMatchObject({status: "success", message: "Encoded URL"})
+                expect(res.body.data).toHaveProperty("url", expect.stringMatching(/https?:\/\/(?:w{1,3}\.)?[^\s.]+(?:\.[a-z]+)*(?::\d+)?(?![^<]*(?:<\/\w+>|\/?>))/))
+                expect(res.body.data.url.split('/')[3].length).toEqual(6)
+                done()
+            })
+    })
 });
